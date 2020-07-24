3f4fb48f0fde3a48b752807e96f1e14a91517f0e
Vicic95 <46875618+Vicic95@users.noreply.github.com>
2020-07-24 14:46:25 +0200

    Add files via upload

--- Java/12. metod/src/metod/Metod.java
+++ Java/12. metod/src/metod/Metod.java
@@ -7,12 +7,20 @@ class Osoba {
     String prezime;
     int godine;
     
-    //Funkcije/metode
+    //Metode mogu biti staticke(klasne) i nestaticke(objektne)
+    //staticke pripadaju celoj klasi i pozivaju se ImeKlase.Metod(parametri)
+    //nestaticke pripadaju pojedinacnim objektima i pozivaju se uz odgovarajuce objekte - ObjekatMetod(parametri)
+    void pozdrav() {
+    	System.out.println("Zdravo");
+    }
+
+    //Preopterecenje metoda - Overloading je situacija kada imamo u klasi dve metoda sa istim imenom
+    //ali razlicitim brojem parametara ili razlicitim parametrima
     void info() {
     	System.out.println("Podaci- Ime: "+ime+" ,Prezime: "+prezime+" ,Godine: "+godine);
     }
-    void pozdrav() {
-    	System.out.println("Zdravo");
+    void info(String arg) {
+    	System.out.println(arg +" Podaci- Ime: "+ime+" ,Prezime: "+prezime+" ,Godine: "+godine);
     }
 }
 public class Metod {
@@ -35,6 +43,7 @@ public class Metod {
         
         osoba2.pozdrav();
         osoba2.info();
+        osoba2.info("Overloading");
         
         System.out.println(osoba2.ime);
 
