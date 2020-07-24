cb17f3db1fcb85ebe317d54a0e3b765351ea5baf
Vicic95 <46875618+Vicic95@users.noreply.github.com>
2020-07-24 14:52:33 +0200

    Add files via upload

--- Java/17. inheritance/src/inheritance/Inheritance.java
+++ Java/17. inheritance/src/inheritance/Inheritance.java
@@ -3,6 +3,10 @@ package inheritance;
 public class Inheritance {
 
 	public static void main(String[] args) {
+		//Ideja nasledjivanja je da se mogu kreirati nove klase koje u postpunosti preuzimaju
+		//funkcionalnosti postoje klase, ali i dodaje svoje atribute i metoda
+		//Klasa koja nasledjuje drugu klasu se naziva potklasa
+		//Klasa koja se nasledjuje se naziva natklasa
 		Osoba osoba1 = new Osoba("Zika","Ziks",46);
         Osoba osoba2 = new Osoba("Mika","Miks",21);
         OsobaNasl nasl = new OsobaNasl("Jova","Jovin",67);
--- Java/17. inheritance/src/inheritance/OsobaNasl.java
+++ Java/17. inheritance/src/inheritance/OsobaNasl.java
@@ -1,7 +1,9 @@
 package inheritance;
 
 public class OsobaNasl extends Osoba {
-
+	//Potklasa klase Osoba, nasledjivanje se izvrsa uz pomoc extends
+	//Klasa moze iati samo jednu natklasu, ali i neogranicen broj potklasa
+	//Potklasa se obraca svojoj natklasi uz pomoc super
 	public OsobaNasl(String ime, String prezime, int godine) {
 		super(ime, prezime, godine);
 	}
