4d72214b506df2ca4803be899b96ceefe910c1da
Vicic95 <46875618+Vicic95@users.noreply.github.com>
2020-07-24 14:49:25 +0200

    Add files via upload

--- Java/14. constructors/src/constructors/Constructors.java
+++ Java/14. constructors/src/constructors/Constructors.java
@@ -3,10 +3,15 @@ package constructors;
 
 class Osoba {
     
-    // Instantne promenljive
+    // Instancne promenljive
     String ime;
     String prezime;
     int godine;
+    //Konstruktor je specijalni metod, koji se koristi za inicijalizaciju objekta
+    //Poziva se u trenutku kreiranja objekta
+    //Ako se ne kreira konstruktor, program poziva podrazumevani konstruktor
+    //this se koristi kao referenca na objekat klase , u konstruktoru se koristi uz objektne atribute
+    //this() se koristi kada klasa ima vise konstruktora, da bi se koristio postojeci konstruktor
     public Osoba() {
 		this("Zika","Ziks",45);
 		System.out.println("Constructor");
@@ -31,12 +36,17 @@ class Osoba {
 public class Constructors {
 
 	public static void main(String[] args) {
+		
+		//Kreiranje sa razlicitm konstruktorima iste klase
 		Osoba osoba1 = new Osoba();
 		System.out.println(osoba1.prezime);
+		
 		Osoba osoba2 = new Osoba("Mika");
 		System.out.println(osoba2.godine);
+		
 		Osoba osoba3 = new Osoba("Jova","Jovin");
 		System.out.println(osoba3.godine);
+		
 		Osoba osoba4 = new Osoba("Lola","Lolin",45);
 		System.out.println(osoba4.godine);
 		
