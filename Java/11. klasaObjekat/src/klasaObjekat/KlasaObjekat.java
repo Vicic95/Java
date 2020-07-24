ac7742f3eff4b3c4422e51f500b7a75ebfd55610
Vicic95 <46875618+Vicic95@users.noreply.github.com>
2020-07-24 14:44:46 +0200

    Add files via upload

--- Java/11. klasaObjekat/src/klasaObjekat/KlasaObjekat.java
+++ Java/11. klasaObjekat/src/klasaObjekat/KlasaObjekat.java
@@ -1,26 +1,45 @@
 package klasaObjekat;
 
+//Ova klasa definise novi tip podatak koji se zove Kutija
 class Osoba {
-    
-    // Instancne promenljive
+    //Klasa je sablon po kom se kreiraju objekti
+	//Objekat je instanca odnosto primerak neke klase
+    // Promenljive instance - Svaki objekat sadrzi sopstvenu kopiju tih promenljivih
     String ime;
     String prezime;
     int godine;
     
-    //Funkcije/metode
 }
 
 public class KlasaObjekat {
 
 	public static void main(String[] args) {
+		
+		//Ovo je promenljiva referentnog tipa, ona sadrzi referencu na podatke
+		//Promenljiva primitivnog tipa sadrzi binarnu reprezentaciju neke vrednosti
+		//A promenljiva referentnog tipa sazdrzi binarnu reprezentaciju adrese nekog objekta
 		// Kreiranje objekta osoba pomocu klase Osoba
-        Osoba osoba1 = new Osoba();    
-        osoba1.ime = "Pera";
+		//Prvo se deklarise promenljiva tipa Osoba, ta promenljiva pokazuje na objekat
+		//Kreira se stvarna fizicka kopija objekta koja se dodeljuje toj promenljivoj, pomocu new
+		//New alocira memoriju za objekat i vraca rederencu na taj memorijski blok
+		//Ta referenca se smesta u promenljivu
+		Osoba osoba1 = new Osoba();
+		
+		//Svaki objekat ima stanje,ponasanje i identitet
+        //Stanja predstavljaju atributi sa svojim vrednostima
+        //Ponasanje se predstavlja pomocu metoda
+        //Identitet se odnosi na svojstvo koje ga razlikuje od drugih objekata
+ 
+		//Dodela vrednosti atributima
+		osoba1.ime = "Pera";
         osoba1.prezime = "Peric";
         osoba1.godine = 37;
         
-        // Kreiranje drugog objekta osoba
-        Osoba osoba2 = new Osoba();
+        //osoba2 sadrzi referencu na objekat tipa Osoba
+        //Ta promenljiva ima podrazumevanu vrednost null i ne pokazuje ni na jedan objekat
+        Osoba osoba2;
+        //Kreira se objekat u memoriji, pa se promenljivoj osoba2 dodeljuje referenca na njega
+        osoba2 = new Osoba();
         osoba2.ime = "Zika";
         osoba2.prezime = "Zikic";
         osoba2.godine = 20;
