6d2adfee429f85fd04db625a90c1033dcb4cb1cd
Vicic95 <46875618+Vicic95@users.noreply.github.com>
2020-07-24 14:57:59 +0200

    Add files via upload

--- Java/21. encapsulation/src/encapsulation/Encapsulation.java
+++ Java/21. encapsulation/src/encapsulation/Encapsulation.java
@@ -3,6 +3,11 @@ package encapsulation;
 public class Encapsulation {
 
 	public static void main(String[] args) {
+		//Enkapsulacija je skrivanje podataka od spoljnih pristupa
+		//Skrivanej podataka se postize uz pomoco private
+		//Pristupa im se samo uz pomoc javnih metoda- getera i setera
+		//Get sluzi za dohvatanje podataka - vraca vrednost podataka
+		//Set sluzi za postavljanje nove vrednosti promenljive
 		Osoba osoba = new Osoba();
 		OsobaNasl osobaNasl = new OsobaNasl();
 		
